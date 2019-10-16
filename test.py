2a67f3746fed1e3ce8ccb8a1ce5fdc85f8bdf2f1
yadlra <yadi.sb1@gmail.com>
2019-10-16 09:24:56 +0100

    add test for initial dead cell state

--- test.py
+++ test.py
@@ -5,3 +5,7 @@ import game_of_life
 class TestGame(unittest.TestCase):
     def test_initialize_board(self):
         game_of_life.Game(10, 10)
+
+    def test_initial_state_cell_dead(self):
+        board = game_of_life.Game(1, 1)
+        self.assertEqual(board.get_cell(0, 0), False)
