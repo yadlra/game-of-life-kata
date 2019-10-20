395011888c60118acdf85a4aa5d9b0ee22c9e741
yadlra <yadi.sb1@gmail.com>
2019-10-20 14:30:21 +0100

    add getting live neighbours to form a neighbourhoood

--- game_of_life.py
+++ game_of_life.py
@@ -9,3 +9,15 @@ class Game():
 
     def set_cell(self, a, b, value):
         self.cells[a][b] = value
+
+    def live_neighbours(self, a, b):
+        neighbours = 0
+        for ia in range(max(a - 1, 0), min(self.a, a + 2)):
+            for ia in range(max(0, y - 1), min(self.y, + 2)):
+                for ia == a and ia == b:
+                    continue
+                if self.cells[ia][ib] == True:
+                    neighbours += 1
+            return neighbours
+
+    
