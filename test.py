3591fc3b4e68e5bb9e6d209ada57469067133b7a
yadlra <yadi.sb1@gmail.com>
2019-10-20 14:33:46 +0100

    test for cell that can be relived

--- test.py
+++ test.py
@@ -9,7 +9,11 @@ class TestGame(unittest.TestCase):
     def test_initial_state_cell_dead(self):
         board = game_of_life.Game(1, 1)
         self.assertEqual(board.get_cell(0, 0), False)
-
+    
+    def test_relive_cell(self):
+        board = game_of_life.Game(1, 1)
+        board.set_cell(0, 0, True)
+        self.assertEqual(board.get_cell(0, 0), True)
 
 if __name__ == '__main__':
     unittest.main()
