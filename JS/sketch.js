94ce7f0632c6a54cb23bba5f0627d5231675a20e
yadlra <yadi.sb1@gmail.com>
2019-10-21 12:12:51 +0100

    add setup function for grid

--- JS/sketch.js
+++ JS/sketch.js
@@ -2,9 +2,21 @@ function make2DArray(cols, rows) {
   let arr = new Array(cols);
   for (let i = 0; i < arr.length; i++) {
     arr[i] = new Array(rows);
-  }
+  } //make the array
+  return arr; //now return it
 }
 //This will make an empty 2D array with nothing in it
 let grid;
 let cols = 10;
 let rows = 10;
+
+function setUp() {
+  grid = make2DArray(10, 10);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      //now I can do a nested loop so that >
+      grid[i][j] = floor(random(2)); //this is a nested loop that will make the 2d array that is empty
+      //& iterate over every column and row and fill each with a random number 0 or 1
+    }
+  }
+}
