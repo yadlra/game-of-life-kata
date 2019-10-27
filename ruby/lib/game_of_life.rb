1201ec0a68b3220d825c9deb3f11a848162ee1f9
yadlra <yadi.sb1@gmail.com>
2019-10-27 18:38:42 +0000

    pass second test for reproduing an object?

--- ruby/lib/game_of_life.rb
+++ ruby/lib/game_of_life.rb
@@ -1,5 +1,16 @@
 class Cell
-    def neighbours_count
-        0
+    attr_accessor :x, :y
+
+    def initialize(x=0, y=0)
+        @x = x
+        @y = y
+    end 
+
+    def neighbours
+        []
+    end
+    
+    def reproduce_at(x, y)
+        Cell.new(x, y)
     end 
 end 
--- ruby/spec/game_of_life_spec.rb
+++ ruby/spec/game_of_life_spec.rb
@@ -1,8 +1,20 @@
 require 'game_of_life.rb'
 
 describe 'game of life' do 
+  context "cell methods" do
+    subject { Cell.new }
+
+    it "returns a cell object" do
+      cell = subject.reproduce_at(3,5)
+      expect(cell.is_a?(Cell)).to be true
+      expect(cell.x).to eq 3
+      expect(cell.y).to eq 5
+    end 
+  end 
+        
+
   it 'any live cell with less than 2 live neighbors, dies' do
-  cell = Cell.new
-  expect(cell.neighbours_count).to eq(0)
+    cell = Cell.new
+    expect(cell.neighbours.count).to eq(0)
   end 
 end 
