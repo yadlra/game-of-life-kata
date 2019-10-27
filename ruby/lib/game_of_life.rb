356f166d4d4c601c5af6fa84cfc6fa1d98abeb36
yadlra <yadi.sb1@gmail.com>
2019-10-27 19:11:39 +0000

    test for cells alignment adding to neighbours array

--- ruby/lib/game_of_life.rb
+++ ruby/lib/game_of_life.rb
@@ -1,16 +1,32 @@
 class Cell
-    attr_accessor :x, :y
+    attr_accessor :grid, :x, :y
 
-    def initialize(x=0, y=0)
+    def initialize(grid, x=0, y=0)
+        @grid = grid
         @x = x
         @y = y
+        grid.cells << self 
     end 
 
     def neighbours
-        []
+        @neighbours = []
+        grid.cells.each do |cell| 
+             if self.x == cell.x && self.y == cell.y - 1
+                @neighbours << cell
+             end 
+        end 
+        @neighbours
     end
     
     def reproduce_at(x, y)
-        Cell.new(x, y)
+        Cell.new(grid, x, y)
+    
+    end 
+end 
+
+class Grid
+    attr_accessor :cells
+    def initialize
+        @cells = []
     end 
 end 
--- ruby/spec/game_of_life_spec.rb
+++ ruby/spec/game_of_life_spec.rb
@@ -1,6 +1,7 @@
 require 'game_of_life.rb'
 
-describe 'game of life' do 
+describe 'game of life' do
+    let(:grid) { Grid.new } 
   context "cell methods" do
     subject { Cell.new }
 
@@ -9,12 +10,18 @@ describe 'game of life' do
       expect(cell.is_a?(Cell)).to be true
       expect(cell.x).to eq 3
       expect(cell.y).to eq 5
+      expect(cell.grid).to eq subject.grid
     end 
   end 
+
+  it "finds a neighbour north" do
+    cell = subject.reproduce_at(0, 1)
+    expect(subject.neighbours.count).to eq(1)
+  end
         
 
   it 'any live cell with less than 2 live neighbors, dies' do
-    cell = Cell.new
+    cell = Cell.new(grid)
     expect(cell.neighbours.count).to eq(0)
   end 
 end 
